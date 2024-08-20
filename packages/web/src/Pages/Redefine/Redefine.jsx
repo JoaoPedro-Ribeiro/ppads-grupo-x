a76223c861f8bf717b0d5eef46ca73ccfc467be3
João Pedro Mantovani <jp_mantovanii@hotmail.com>
2024-08-19 21:49:44 -0300

    GRUPOX-37: Criação da página redefine e organização das variáveis de cores

---
+++ packages/web/src/Pages/Redefine/Redefine.jsx
@@ -0,0 +1,38 @@
+import './Redefine.css'
+
+import { useState } from 'react';
+
+
+
+const Redefine = () => {
+    const [username, setUsername] = useState("");;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(username)
+
+        console.log("Envio");
+    }
+
+  return (
+  <main>
+    <div className='Login'>
+      <h1>OLÁ,<br></br>BIBLIOTECÁRIO(A)!</h1>
+      <div className='formRedefine'>
+        <div className='containerRedefine'>
+          <form onSubmit={handleSubmit}>
+          <h2>REDEFINIÇÃO DE SENHA</h2>
+          <div className='input-fieldRedefine'>
+            <input type='email' placeholder='EMAIL' onChange={(e) => setUsername(e.target.value)}></input>
+          </div>
+          <button>REDEFINIR</button>
+          </form>
+        </div>  
+      </div>
+    </div>
+  </main>
+  )
+}
+
+export default Redefine
--- packages/web/src/Routes/Routes.jsx
+++ packages/web/src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "../Pages/Login/Login.jsx"
+import Redefine from "../Pages/Redefine/Redefine.jsx";
 
 const RoutesApp = () => {
     return (
@@ -9,6 +10,7 @@ const RoutesApp = () => {
                 <Routes>
                     <Route exact path="/login" element={<Login />}/>
                     <Route path="*" element={<Login />}/>
+                    <Route exact path="/redefine" element={<Redefine />}/>
                 </Routes>
             </Fragment>
         </BrowserRouter>
