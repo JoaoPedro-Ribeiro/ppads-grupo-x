054a6eb98b133cb85d75890db02f7d146ebc0697
João Pedro Mantovani <jp_mantovanii@hotmail.com>
2024-09-12 08:29:48 -0300

    GRUPOX-71: Configuração JWT

--- packages/server/src/users/users.service.ts
+++ packages/server/src/users/users.service.ts
@@ -13,4 +13,8 @@ export class UsersService {
     this.users.push(newUser)
     console.log(this.users)
   }
+
+  findByEmail(email: string): UserDto | null {
+    return this.users.find(user => user.email === email)
+  }
 }
