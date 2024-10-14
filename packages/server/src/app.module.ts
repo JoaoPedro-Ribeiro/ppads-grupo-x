e6d456b9011fd7fd6a450e7496f189abedf4419a
João Pedro Mantovani <jp_mantovanii@hotmail.com>
2024-10-14 17:54:28 -0300

    GRUPOX-107: Criação da rota de categories

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,12 +3,14 @@ import { UsersModule } from './modules/users/users.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
 import { DynamodbModule } from './modules/dynamodb/dynamodb.module'
+import { CategoriesModule } from './modules/books/categories/categories.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     AuthModule,
+    CategoriesModule,
     DynamodbModule]
 })
 export class AppModule {}
