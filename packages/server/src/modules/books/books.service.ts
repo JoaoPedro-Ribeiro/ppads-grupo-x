bc67703dedcd9ba84a76b2ac1cf316e0f3297025
João Pedro Ribeiro <contato.joaopribeiro@gmail.com>
2024-11-02 10:31:59 -0300

    GRUPOX-120: lint fix

--- packages/server/src/modules/books/books.service.ts
+++ packages/server/src/modules/books/books.service.ts
@@ -3,8 +3,7 @@ import { InputCreateBookDto } from './dto/inputCreateBook.dto'
 import { BooksRepository } from '../dynamodb/repositories/booksRepository'
 import { v4 as uuid } from 'uuid'
 import { InputUpdateBookDto } from './dto/inputUpdateBook.dto'
-import { BookModel } from './book.schema';
-
+import { BookModel } from './book.schema'
 
 @Injectable()
 export class BooksService {
@@ -64,9 +63,9 @@ export class BooksService {
 
     return { success: true, message: 'Book updated successfully' }
   }
-  
+
   async findBooksByTitle(title: string): Promise<any> {
-    const books = await BookModel.scan('title').contains(title).exec();
-    return books;
+    const books = await BookModel.scan('title').contains(title).exec()
+    return books
   }
 }
