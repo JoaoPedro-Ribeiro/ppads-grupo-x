2f7c9a189a64bb066f0010c7c7378c774343c5a2
João Pedro Ribeiro <contato.joaopribeiro@gmail.com>
2024-11-02 11:57:14 -0300

    GRUPOX-125: removing error controller

--- packages/server/src/modules/errors/errors.module.ts
+++ packages/server/src/modules/errors/errors.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ErrorsService } from './errors.service'
-import { ErrorsController } from './errors.controller'
 
 @Module({
-  controllers: [ErrorsController],
   providers: [ErrorsService],
   exports: [ErrorsService]
 })
