dc2f776f0fe0c6fd376f26434b100914b3b6fd65
João Pedro Ribeiro <contato.joaopribeiro@gmail.com>
2024-11-03 15:56:45 -0300

    GRUPOX-127: adding more errors to errorService

--- packages/server/src/modules/errors/errors.service.ts
+++ packages/server/src/modules/errors/errors.service.ts
@@ -5,10 +5,55 @@ export class ErrorsService {
   static userNotFound() {
     return new HttpException(
       {
+        errorCode: 'X-001',
         success: false,
         message: 'User not found!'
       },
       HttpStatus.NOT_FOUND
+    )
+  }
+
+  static emailAlreadyInUse() {
+    return new HttpException(
+      {
+        errorCode: 'X-002',
+        success: false,
+        message: 'Email Already In Use'
+      },
+      HttpStatus.CONFLICT
+    )
+  }
+
+  static failToCreateUser() {
+    return new HttpException(
+      {
+        errorCode: 'X-003',
+        success: false,
+        message: 'Fail To Create User'
+      },
+      HttpStatus.BAD_REQUEST
+    )
+  }
+
+  static failToDeleteUser() {
+    return new HttpException(
+      {
+        errorCode: 'X-004',
+        success: false,
+        message: 'Fail To Delete User'
+      },
+      HttpStatus.BAD_REQUEST
+    )
+  }
+
+  static dynamoError() {
+    return new HttpException(
+      {
+        errorCode: 'X-010',
+        success: false,
+        message: 'Dynamo Error!'
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR
     )
   }
 }
