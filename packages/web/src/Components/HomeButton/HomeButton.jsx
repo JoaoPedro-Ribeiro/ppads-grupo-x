4df85b313b1feacb4c3df98df98187adb8899c5a
João Pedro Mantovani <jp_mantovanii@hotmail.com>
2024-11-06 13:40:38 -0300

    GRUPOX-103: Fix erro merge

--- packages/web/src/Components/HomeButton/HomeButton.jsx
+++ packages/web/src/Components/HomeButton/HomeButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
+import logo from '../../Assets/logo.svg'
 import './HomeButton.css'
 
 function HomeButton() {
